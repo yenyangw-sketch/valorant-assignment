d46d2922684a934da8bcf117b160c9c138ac98e8
yenyangw-sketch <yenyangw@gmail.com>
2026-07-14 11:45:51 +0800

    Delete frontend directory

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api',
-  },
-};
-
-module.exports = nextConfig;
